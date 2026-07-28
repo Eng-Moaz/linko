1846c45bd6b00d7ec38eeecf94377d8ffa3552e9
Eng-Moaz <156330236+Eng-Moaz@users.noreply.github.com>
2026-07-28 09:17:58 +0300

    added request counter

--- server.go
+++ server.go
@@ -25,7 +25,7 @@ func newServer(store store.Store, port int, cancel context.CancelFunc, logger *s
 
 	srv := &http.Server{
 		Addr:    fmt.Sprintf(":%d", port),
-		Handler: requestIdMiddleware(requestLogger(logger)(mux)),
+		Handler: metricsMiddleware(requestIdMiddleware(requestLogger(logger)(mux))),
 	}
 
 	s := &server{
